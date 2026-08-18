e658fc65e000ff94eb372a1dda3f9d6067a8d6f5
hghh345 <trystorytelling@gmail.com>
2026-08-18 22:18:09 +0100

    d

--- upload.js
+++ upload.js
@@ -3,14 +3,6 @@
 /* upload.js */
 /* -------------------------------- */
 
-const SUPABASE_URL = "https://cvuyybeihtvhygxukbuj.supabase.co/rest/v1/";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable__Fve3_Cvt8pR3mrISv0Vhg_g4LpT0gD";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
-
 /* ------------------------------ */
 /* Elements */
 /* ------------------------------ */
